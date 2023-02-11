b2fd84fd28642531664eb144f917ed87f592932d
MyFirstRepository <narek177@gmail.com>
2023-02-11 23:22:44 +0300

    Переписал коды

--- VoiceTexterBot/Program.cs
+++ VoiceTexterBot/Program.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
+using VoiceTexterBot.Configuration;
+using VoiceTexterBot.Services;
+using VoiceTexterBot.Controllers;
 
 namespace VoiceTexterBot
 {
@@ -12,23 +15,41 @@ namespace VoiceTexterBot
         public static async Task Main()
         {
             Console.OutputEncoding = Encoding.Unicode;
-            
+
             var host = new HostBuilder()
-                .ConfigureServices((hostContext, services) => ConfigureServices(services)) 
-                .UseConsoleLifetime() 
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))
+                .UseConsoleLifetime()
                 .Build();
 
             Console.WriteLine("Сервис запущен");
-            
+
             await host.RunAsync();
             Console.WriteLine("Сервис остановлен");
         }
 
-        static void ConfigureServices(IServiceCollection services)        {
-            
-            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6190187242:AAGo97DWjHJ-FMa-vKDruEd_CJOuO8f_zYA"));
-            
+        static void ConfigureServices(IServiceCollection services)
+        {
+            AppSettings appSettings = BuildAppSettings();
+            services.AddSingleton(BuildAppSettings());
+
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Подключаем контроллеры сообщений и кнопок
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient(appSettings.BotToken));
             services.AddHostedService<Bot>();
+        }
+        
+        static AppSettings BuildAppSettings()
+        {
+            return new AppSettings()
+            {
+                BotToken = "6190187242:AAGo97DWjHJ-FMa-vKDruEd_CJOuO8f_zYA"
+            };
         }
     }
 }
